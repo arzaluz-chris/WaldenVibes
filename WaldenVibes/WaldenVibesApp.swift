3f56a283bc822618a947ab8861d8c967f0c7d099
arzaluz-chris <23008381@es.uveg.edu.mx>
2025-06-23 20:44:30 -0600

    Initial Commit

---
+++ WaldenVibes/WaldenVibesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaldenVibesApp.swift
+//  WaldenVibes
+//
+//  Created by Christian Arzaluz on 23/06/25.
+//
+
+import SwiftUI
+
+@main
+struct WaldenVibesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
